49d7f0785227a87066fe2c2118fcf802b3867674
anastasiia <a.o.olifir@gmail.com0>
2025-03-18 19:57:53 +0200

    return settings for htzner

--- tasks.py
+++ tasks.py
@@ -10,27 +10,28 @@ def run(ctx):
     ctx.run('./manage.py collectstatic --noinput')
 
     # comment for hetzner
-    print('Run WSGI server with gunicorn')
-    ctx.run('gunicorn config.wsgi --bind 0.0.0.0:8000')
+    # print('Run WSGI server with gunicorn')
+    # ctx.run('gunicorn config.wsgi --bind 0.0.0.0:8000')
     # heroku supports gunicorn, hetzner supports uwsgi
-    # command = ('uwsgi --http 0.0.0.0:8080 --master'
-    #         ' --module "config.wsgi:get_wsgi_application()"'
-    #         ' --processes=5'
-    #         ' --offload-threads 4'
-    #         ' --enable-threads'
-    #         ' --static-map /static=/static'
-    #         ' --static-map /media=/media'
-    # )
-    #
-    # if os.getenv('PY_AUTORELOAD'):
-    #     command += ' --py-autoreload 1'
-    #
-    # # if os.getenv('BASICAUTH'):
-    # #     command += ' --route "^/--basicauth:BA,{0}"'.format(os.getenv('BASICAUTH'))
-    #
-    # if os.getenv('ENV', "dev") == 'dev':
-    #     command += ' --honour-stdin'
-    # else:
-    #     command += ' --harakiri 30'
 
-    # ctx.run(command)
+    command = ('uwsgi --http 0.0.0.0:8080 --master'
+            ' --module "config.wsgi:get_wsgi_application()"'
+            ' --processes=5'
+            ' --offload-threads 4'
+            ' --enable-threads'
+            ' --static-map /static=/static'
+            ' --static-map /media=/media'
+    )
+
+    if os.getenv('PY_AUTORELOAD'):
+        command += ' --py-autoreload 1'
+
+    # if os.getenv('BASICAUTH'):
+    #     command += ' --route "^/--basicauth:BA,{0}"'.format(os.getenv('BASICAUTH'))
+
+    if os.getenv('ENV', "dev") == 'dev':
+        command += ' --honour-stdin'
+    else:
+        command += ' --harakiri 30'
+
+    ctx.run(command)
