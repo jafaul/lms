5956025978a640f4b774367a4e00715c78e67ff2
anastasiia <a.o.olifir@gmail.com0>
2025-03-18 21:36:27 +0200

    del whitenoise in settings prod

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -4,9 +4,3 @@ DEBUG = False
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 SITE_URL = "https://lms-server-e5b12b7c980b.herokuapp.com/"
-
-MIDDLEWARE += [
-    'whitenoise.middleware.WhiteNoiseMiddleware',
-]
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
