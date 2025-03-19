d5632f183b8d1c0c98221c0a729d3272ef3e013d
anastasiia <a.o.olifir@gmail.com0>
2025-03-19 14:28:22 +0200

    change site url

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -3,5 +3,5 @@ from .docker import *
 DEBUG = False
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
-SITE_URL = "http://3.120.187.32:8080"
-# SITE_URL = "http://localhost:8080"
+# SITE_URL = "http://3.120.187.32:8080"
+SITE_URL = "http://localhost:8080"
