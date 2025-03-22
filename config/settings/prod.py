7a4d346d0a024f9bee43df801c671c0cb283be36
anastasiia <a.o.olifir@gmail.com0>
2025-03-22 18:39:21 +0200

    change prod url

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -2,7 +2,7 @@ from .docker import *
 
 DEBUG = False
 
-SITE_URL = "http://13.53.119.20:8080"
+SITE_URL = "http://16.16.207.86:8080"
 
 MIDDLEWARE += [
     'whitenoise.middleware.WhiteNoiseMiddleware'
