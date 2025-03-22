bce9167d36b1e7f706a5a4b9ee58e2ae240312e5
anastasiia <a.o.olifir@gmail.com0>
2025-03-22 17:54:21 +0200

    change site url

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -2,7 +2,7 @@ from .docker import *
 
 DEBUG = False
 
-SITE_URL = "http://16.171.11.192:8080"
+SITE_URL = "http://13.53.119.20:8080"
 
 MIDDLEWARE += [
     'whitenoise.middleware.WhiteNoiseMiddleware'
